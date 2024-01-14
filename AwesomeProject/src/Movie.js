6b00286a61884070a343b23a6d6147345caa923b
Marydaydreamed <mholy9002@gmail.com>
2024-01-14 17:14:42 +0800

    movie

--- AwesomeProject/src/Movie.js
+++ AwesomeProject/src/Movie.js
@@ -1,55 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { StatusBar } from 'expo-status-bar'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Movie from './Movie'
 
-const Movie = () => {
-    const [boxes, setBoxes] = useState(['','','',''])
-  return (
+const Movies = () => {
+    const movies = [
+         {
+             name: 'Avengers1',
+             desc: 'Avengers: Endgame is a 2019 American superhero film based on the Marvel Comics superhero team the Avengers. Produced by Marvel Studios and distributed by ...',
+             img: require('../avengers (1).jpg')
+         },
+         {
+             name: 'Iron man',
+             desc: 'Avengers: Endgame is a 2019 American superhero film based on the Marvel Comics superhero team the Avengers. Produced by Marvel Studios and distributed by ...',
+             img: require('../ironman (1).jpg')
+         },
+        {
+            name: 'Thor: Love and Thunder',
+            desc: 'Thor: Love and Thunder is a 2022 American superhero film based on Marvel Comics featuring the character Thor. ',
+            img: require('../thor_love_and_thunder_dc (1).jpg')
+        }
+    ]
+    return (
+        <ScrollView>
+          
 
-    <View style={styles.container}>
-        <Text style={styles.text}>Таньд санал болгох кино.</Text>
-        <View style={styles.movie}>
-     {boxes.map((box, i)=>
-     <View style={styles.box}>
-     </View>
-     )}
-      </View>
-    </View>
-  )
+            <ScrollView contentContainerStyle={styles.container}>
+                {movies.map(movie =>
+                    <Movie name={movie.name} desc={movie.desc} img={movie.img} />
+                )}
+            </ScrollView>
+        </ScrollView>
+
+    )
 }
 
-export default Movie
+export default Movies
 
 const styles = StyleSheet.create({
-container:{
-flex:1,
-backgroundColor:'white',
-alignItems:'center'
-},
-box:{
-    width:160,
-    height:250,
-    backgroundColor:'lightblue',
-    marginTop: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-        width: 0,
-        height: 12,
-    },
-    shadowOpacity: 0.58,
-    shadowRadius: 16.00,
-   
-    elevation: 24,
-
-},
-movie:{
-    flexDirection:'row',
-justifyContent:'space-between',
-flexWrap:'wrap',
-marginRight:20,
-marginLeft:20
-},
-text:{
-fontSize:20,
-}
+    container: {
+        alignItems: 'center'
+    }
 })
